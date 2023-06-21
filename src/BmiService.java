b39e05f99e4edbfb74769d2da24551609a247072
Roman Kochnev <romaslog@icloud.com>
2023-06-21 23:05:40 +0500

    Result

---
+++ src/BmiService.java
@@ -0,0 +1,9 @@
+public class BmiService {
+    public int calculate(double height, int weight) {
+        int result = (int)((int) weight / (height * height));
+
+        return result;
+
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
+import java.lang.module.FindException;
+
 public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double height = (int) 1.87;
+        int weight = 98;
+        int Bmi = service.calculate(1.87,98);
+
+
+        System.out.println("Индекс массы тела:");
+        System.out.println(service.calculate(1.80,80));
+
+        System.out.println("Индекс массы тела:");
+        System.out.println(service.calculate(2.10,120));
+    }
 }
