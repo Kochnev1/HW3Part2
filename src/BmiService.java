b3382dbe77f603915b1027c64bd31ca378e72b78
Roman Kochnev <romaslog@icloud.com>
2023-06-22 20:52:40 +0500

    Reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,9 +1,6 @@
 public class BmiService {
     public int calculate(double height, int weight) {
-        int result = (int)((int) weight / (height * height));
-
+        int result = (int) ((int) weight / (height * height));
         return result;
-
-
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,12 @@ public class Main {
         BmiService service = new BmiService();
         double height = (int) 1.87;
         int weight = 98;
-        int Bmi = service.calculate(1.87,98);
-
-
-        System.out.println("Индекс массы тела:");
-        System.out.println(service.calculate(1.80,80));
+        int Bmi = service.calculate(1.87, 98);
 
         System.out.println("Индекс массы тела:");
-        System.out.println(service.calculate(2.10,120));
+        System.out.println(service.calculate(1.80, 80));
+
+        System.out.println("Индекс массы тела:");
+        System.out.println(service.calculate(2.10, 120));
     }
 }
